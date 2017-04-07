209a77a62d82d4df60dac64f931fe9d03bb68537
compassghost <compassghost@gmail.com>
2017-04-07 19:49:20 -0400

    Fix one-second offset use ceil instead of floor for seconds.

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function createTimer(timerId, targetDay) {
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var seconds = Math.ceil((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
   document.getElementById(timerId).innerHTML = pad(days, 2) + "d " + pad(hours, 2) + "h "
