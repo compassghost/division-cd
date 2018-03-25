85d5850a2b8300a82198d1f5a090d97d9f6119bb
compassghost <compassghost@gmail.com>
2018-03-25 01:10:42 -0400

    Add more comments and optimizations in prep for rewrite.

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,11 +1,18 @@
+//Pads value n with chars (default '0') to width
 function pad(n, width, z) {
   z = z || '0';
   n = n + '';
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
+function padTime(n) {
+	return pad(n, 2);
+}
+
+//default 10-padding of nbsp;
 var padding = textPad(10);
 
+//creates a string of nbsp; of length count
 function textPad(count) {
 	var pad = "";
 	for(i = 0; i < count; i++) {
@@ -14,10 +21,12 @@ function textPad(count) {
 	return pad;
 }
 
+//creates an auto-refreshing display for timer, default time is 1000ms
 function deployTimer(timer) {
 	setInterval(timer, 1000);
 }
 
+//Timer function that displays displayText inside a TR timerID counting towards targetDay
 function createTimer(displayText, timerId, targetDay) {
   if(displayText=== undefined)  {
 	  displayText= "";
@@ -43,22 +52,12 @@ function createTimer(displayText, timerId, targetDay) {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById(timerId).innerHTML = "<td>" + displayText+ pad(days, 2) + "d " + pad(hours, 2) + "h "
-  + pad(minutes, 2) + "m " + pad(seconds, 2) + "s </td>";
+  document.getElementById(timerId).innerHTML = "<td>" + displayText+ padTime(days) + "d " + padTime(hours) + "h "
+  + padTime(minutes) + "m " + padTime(seconds) + "s </td>";
 
 }
 
-function createWatch(timerId) {
-  // Get todays date and time
-  var now = new Date();
-
-  // Find the distance between now an the count down date
-
-  // Display the result in the element with id="demo"
-  document.getElementById(timerId).innerHTML = "" + pad(now.getUTCHours(), 2) + ":"
-  + pad(now.getMinutes(), 2) + ":" + pad(now.getSeconds(), 2);
-}
-
+//gets next UTC
 function getNextUTC() {
 	var target = new Date();
 		
@@ -73,6 +72,7 @@ function getNextUTC() {
 	return target;
 }
 
+//gets next UTC month
 function getNextMonth() {
 	var target = new Date();
 	
@@ -85,6 +85,7 @@ function getNextMonth() {
 	return target;
 }
 
+//gets next supply date, either first of 
 function getNextSupply() {
 	var target = new Date();
 	//this month
@@ -97,28 +98,12 @@ function getNextSupply() {
 	}
 	//next month
 	else {
-		target.setUTCDate(1)
-		target.setUTCHours(0);
-		target.setMinutes(0);
-		target.setSeconds(1);
-		target.setMilliseconds(0);
-		target.setUTCMonth(target.getUTCMonth() + 1);
+		return getNextMonth();
 	}
 	return target;
 }
 
-
-function getNextDayPremium() {
-	var date = getNextDay(5);
-	date.setUTCHours(-24+9);
-	
-	if(date.getTime() < new Date().getTime()) {
-		date = getNextDay(4);
-		date.setUTCHours(9);
-	}
-	return date;
-}
-
+//gets next day by week (0-6, UTC standard)
 function getNextDay(dayOfWeek) {
 	var target = getNextUTC();
 	
@@ -128,6 +113,7 @@ function getNextDay(dayOfWeek) {
 	return target;	
 }
 
+//create a UTC date using YMDH
 function createUTCDate(year, month, day, hour) {
 	var date = new Date();
 	date.setUTCFullYear(year);
@@ -141,6 +127,7 @@ function createUTCDate(year, month, day, hour) {
 	return date;
 }
 
+//create a timer timerId with name itemName that spans from startDay to endDay, and removes itself once it expires
 function createEventTimer(itemName, timerId, startDay, endDay) {
 	if(startDay.getTime() > new Date().getTime()) {
 		createTimer(itemName + " starts in", timerId, startDay);
@@ -150,7 +137,15 @@ function createEventTimer(itemName, timerId, startDay, endDay) {
 	}
 	else {
 	    document.getElementById(timerId).innerHTML = "";
+	    document.getElementById(timerId).display = "none";	
 	}
+}
 
 
+function createWatch(timerId) {
+  // Get todays date and time
+  var now = new Date();
+
+  document.getElementById(timerId).innerHTML = "" + padTime(now.getUTCHours()) + ":"
+  + padTime(now.getMinutes()) + ":" + padTime(now.getSeconds());
 }
